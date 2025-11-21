378ca0f740384aa0de9ae7f3dacf846fcbbbe906
ZvonimirSun <hi@iszy.xyz>
2025-11-21 13:06:07 +0800

    feat: 添加error页

---
+++ app/error.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import type { NuxtError } from '#app'
+
+defineProps({
+  error: Object as () => NuxtError,
+})
+</script>
+
+<template>
+  <div>
+    <h1>{{ error?.statusCode }}</h1>
+    <NuxtLink to="/">
+      返回主页
+    </NuxtLink>
+  </div>
+</template>
