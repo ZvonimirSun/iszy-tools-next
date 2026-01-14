32c6f14b13eb234b73237edc60300cafe3a3aaab
ZvonimirSun <hi@iszy.xyz>
2026-01-14 14:00:03 +0800

    feat: 新增公安备案展示

--- app/components/AppFooter.vue
+++ app/components/AppFooter.vue
@@ -6,14 +6,22 @@ const { public: { beian } } = useRuntimeConfig()
 <template>
   <UFooter>
     <template #left>
-      <p class="text-muted text-sm">
-        Copyright © {{ new Date().getFullYear() }}<template v-if="beian">
-          &nbsp;|&nbsp;
+      <div class="text-muted text-sm whitespace-nowrap flex items-center gap-2">
+        Copyright © {{ new Date().getFullYear() }}
+        <template v-if="beian">
+          |
           <ULink to="https://beian.miit.gov.cn/#/Integrated/recordQuery" target="_blank">
             {{ beian.icp }}
           </ULink>
         </template>
-      </p>
+        <template v-if="beian?.gonganNum">
+          |
+          <img class="w-4" src="/images/beian.png">
+          <ULink :to="`https://beian.mps.gov.cn/#/query/webSearch?code=${beian.gonganId}`" target="_blank">
+            {{ beian.gonganNum }}
+          </ULink>
+        </template>
+      </div>
     </template>
     <template #right>
       <template v-if="footer?.links">
