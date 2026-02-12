d89512a8e32b0e94a1895597c0330972b22ec412
ZvonimirSun <hi@iszy.xyz>
2026-02-12 18:21:33 +0800

    feat(hashText): 主页面支持ssr

---
+++ app/pages/hashText.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+const clearText = ref('')
+const encoding = ref('Hex')
+const encodingOptions = [
+  {
+    label: 'Binary (base 2)',
+    value: 'Bin',
+  },
+  {
+    label: 'Hexadecimal (base 16)',
+    value: 'Hex',
+  },
+  {
+    label: 'Base64 (base 64)',
+    value: 'Base64',
+  },
+  {
+    label: 'Base64url (base 64 with url safe chars)',
+    value: 'Base64url',
+  },
+]
+</script>
+
+<template>
+  <div class="max-h-full flex flex-col gap-4">
+    <span>待计算的内容</span>
+    <UTextarea
+      v-model="clearText"
+      class="w-full h-full"
+      resize
+      textarea-class="h-full"
+      placeholder="请输入要计算的文本..."
+    />
+    <span>Digest 编码</span>
+    <USelect v-model="encoding" :items="encodingOptions" />
+    <ContainerToolItem label="结果">
+      <ClientOnly>
+        <HashTextResult :value="clearText" :encoding="encoding" />
+      </ClientOnly>
+    </ContainerToolItem>
+  </div>
+</template>
