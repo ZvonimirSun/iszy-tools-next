53622963b74b82763688011f95eeca015239e895
ZvonimirSun <hi@iszy.xyz>
2026-03-03 18:02:03 +0800

    feat: fallback地址，防止vue-router警告

---
+++ app/pages/[...all].vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <UError
+    class="w-full h-full min-h-full"
+    :error="{
+      statusMessage: '404',
+      message: '对不起，你访问的页面不存在。',
+    }"
+    :clear="{
+      label: '返回首页',
+    }"
+  />
+</template>
