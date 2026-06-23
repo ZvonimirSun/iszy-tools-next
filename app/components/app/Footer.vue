a8f9e9265db90aa77e06ee18009b8ecb7e4d0ee1
ZvonimirSun <hi@iszy.xyz>
2026-06-23 23:10:46 +0800

    fix: 调整页脚居中布局

--- app/components/app/Footer.vue
+++ app/components/app/Footer.vue
@@ -9,29 +9,29 @@ if (since && since.toString() !== year) {
 </script>
 
 <template>
-  <UFooter>
-    <template #left>
-      <div class="text-muted text-sm flex flex-col gap-2">
-        <div class="flex gap-1">
-          <span>Copyright © {{ displayYear }}</span>
-          <span v-if="copyright">{{ copyright }}</span>
-        </div>
-        <template v-if="beian?.enable">
-          <template v-if="beian.icp">
-            <ULink to="https://beian.miit.gov.cn/#/Integrated/recordQuery" target="_blank">
-              {{ beian.icp }}
-            </ULink>
-          </template>
-          <div
-            v-if="beian.gonganNum"
-            class="flex items-center gap-2 whitespace-nowrap"
-          >
-            <img class="w-4" src="~/assets/images/beian.png" alt="公安备案">
-            <ULink :to="`https://beian.mps.gov.cn/#/query/webSearch?code=${beian.gonganId}`" target="_blank">
-              {{ beian.gonganNum }}
-            </ULink>
-          </div>
-        </template>
+  <UFooter
+    :ui="{
+      center: 'text-muted flex flex-col items-center gap-2 text-center text-sm',
+    }"
+  >
+    <div class="flex gap-1">
+      <span>Copyright © {{ displayYear }}</span>
+      <span v-if="copyright">{{ copyright }}</span>
+    </div>
+    <template v-if="beian?.enable">
+      <template v-if="beian.icp">
+        <ULink to="https://beian.miit.gov.cn/#/Integrated/recordQuery" target="_blank">
+          {{ beian.icp }}
+        </ULink>
+      </template>
+      <div
+        v-if="beian.gonganNum"
+        class="flex items-center gap-2 whitespace-nowrap"
+      >
+        <img class="w-4" src="~/assets/images/beian.png" alt="公安备案">
+        <ULink :to="`https://beian.mps.gov.cn/#/query/webSearch?code=${beian.gonganId}`" target="_blank">
+          {{ beian.gonganNum }}
+        </ULink>
       </div>
     </template>
   </UFooter>
